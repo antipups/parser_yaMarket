288a0bc0fde81a2ea5876896856e3294b1d2d09b
antipups <nick.kurkurin@protonmail.com>
2020-04-24 16:23:21 +0300

    Новые прокси + новые хедеры.

--- main_pack/parser.py
+++ main_pack/parser.py
@@ -42,8 +42,20 @@ def parse(dict_of_product):
     all_proxies = parser_proxy.parse_proxy()
         # all_proxies = f.read().split('\n')
     headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9,ru;q=0.8',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive',
+        'Cookie': f'_ym_uid=1579538585539137365; mda=0; yandexuid=4775578911579527928; yuidss=4775578911579527928; i=7fpXxLZtVlknYALqfuHdIqESJ7QKPyrT/p1Ib1NKFr2+GlI6yQ4Nq8EuI7FxxNSTvC5JPW7DUgpjBnX2NCjgeBJhLQE=; my=YwA=; yandex_gid=142; gdpr=0; yandexmarket=48%2CUAH%2C1%2C%2C%2C%2C2%2C0%2C0%2C0%2C0%2C0%2C12%2C0; mOC=1; currentRegionId=142; currentRegionName=%D0%94%D0%BE%D0%BD%D0%B5%D1%86%D0%BA; cycada=vBIVPiL1OivB3zUy7Bun86lEsDT+JL2RpLjQnNHDsg4=; yabs-frequency=/4/0000000000000000/AKgmSB0wGG00/; pof=%7B%22clid%22%3A%5B%222359538%22%5D%2C%22mclid%22%3A%221002%22%2C%22distr_type%22%3A%227%22%2C%22vid%22%3A%22986%22%2C%22opp%22%3A%22900%22%7D; cpa-pof=%7B%22clid%22%3A%5B%222359538%22%5D%2C%22mclid%22%3A%221002%22%2C%22distr_type%22%3A%227%22%2C%22vid%22%3A%22986%22%2C%22opp%22%3A%22900%22%7D; oMaSefD=1; oMaSpfD=1; oMaRefD=1; Session_id=3:1587567800.5.0.1587567800047:uOvH2Q:4c.1|230517873.0.2|4:156007.208530.Ct9WborQwK5ZqYGCgMhyMgHKkSI; sessionid2=3:1587567800.5.0.1587567800047:uOvH2Q:4c.1|230517873.0.2|4:156007.666179.wzxl5em2LjckToAqLylJxzwXt3M; L=WnRGfUNYc2xfWkAFakRaBg5bYgN0cAVxGAcNIUANFBMvBwIAJw==.1587567800.14211.321094.42a247e10a7e1c492bb9a0954f71a90a; yandex_login=nick.kurkurin; skid=7141896761587577073; _ym_d=1587577075; visits=1586198458-1587549787-1587643024; lr=0; dcm=1; _ym_wasSynced=%7B%22time%22%3A1587643027213%2C%22params%22%3A%7B%22eu%22%3A0%7D%2C%22bkParams%22%3A%7B%7D%7D; _ym_isad=2; ugcp=1; yp=1581234933.com.3#1581437288.dq.1#1896019512.multib.1#1584131668.oyu.4775578911579527928#1596427321.szm.1_25%3A1536x864%3A1536x760#1902927800.udn.cDpuaWNrLmt1cmt1cmlu#1612368485.ygu.1#1894898590.yrtsi.1579538590#1587729427.yu.4775578911579527928; ymex=1590235027.oyu.4775578911579527928#1898004678.yrts.1582644678#1894898590.yrtsi.1579538590; oMaSofD=1; oMaFifD=1; spravka=dD0xNTg3NjQzNDY5O2k9MjE3LjE5OS4yMzYuMTM1O3U9MTU4NzY0MzQ2OTI1OTAwNzM2MztoPTNkZTAxZGNhNGU2NGU2MzFjNDM5MDRkYWEwODZlNWJl; uid=AABcdl6h0xFT/QBoBj78Ag==; js=1; first_visit_time={datetime.datetime.now().strftime("%Y-%m-%d")}T{datetime.datetime.now().strftime("%H")}%3A{datetime.datetime.now().strftime("%M")}%3A{datetime.datetime.now().strftime("%S")}%2B03%3A00; fonts-loaded=1; _ym_visorc_160656=b; _ym_visorc_45411513=b; metrika_enabled=1; _ym_metrika_enabled=1; HISTORY_AUTH_SESSION=edf01ebb; parent_reqid_seq=32a2e5f8f7b07ef01b1b51f5713261f1%2Cf56f71597835f3c7d2890719dde73a35%2C5929f9d6bc20b3dc7e7110c643dcfa53%2C4ccf49d0f29fea9142486001ae159458; _ym_metrika_enabled_160656=1; _ym_metrika_enabled_44910898=1; _ym_metrika_enabled_45411513=1; oMaPrfD=1',
+        'Referer': '',
+        'Host': 'market.yandex.kz',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-User': '?1',
+        'Upgrade-Insecure-Requests': '1',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
-        'Cookie': f'_ym_uid=1579538585539137365; mda=0; yandexuid=4775578911579527928; yuidss=4775578911579527928; i=7fpXxLZtVlknYALqfuHdIqESJ7QKPyrT/p1Ib1NKFr2+GlI6yQ4Nq8EuI7FxxNSTvC5JPW7DUgpjBnX2NCjgeBJhLQE=; my=YwA=; yandex_gid=142; gdpr=0; yandexmarket=48%2CUAH%2C1%2C%2C%2C%2C2%2C0%2C0%2C0%2C0%2C0%2C12%2C0; mOC=1; currentRegionId=142; currentRegionName=%D0%94%D0%BE%D0%BD%D0%B5%D1%86%D0%BA; cycada=vBIVPiL1OivB3zUy7Bun86lEsDT+JL2RpLjQnNHDsg4=; yabs-frequency=/4/0000000000000000/AKgmSB0wGG00/; pof=%7B%22clid%22%3A%5B%222359538%22%5D%2C%22mclid%22%3A%221002%22%2C%22distr_type%22%3A%227%22%2C%22vid%22%3A%22986%22%2C%22opp%22%3A%22900%22%7D; cpa-pof=%7B%22clid%22%3A%5B%222359538%22%5D%2C%22mclid%22%3A%221002%22%2C%22distr_type%22%3A%227%22%2C%22vid%22%3A%22986%22%2C%22opp%22%3A%22900%22%7D; oMaSefD=1; oMaSpfD=1; oMaRefD=1; Session_id=3:1587567800.5.0.1587567800047:uOvH2Q:4c.1|230517873.0.2|4:156007.208530.Ct9WborQwK5ZqYGCgMhyMgHKkSI; sessionid2=3:1587567800.5.0.1587567800047:uOvH2Q:4c.1|230517873.0.2|4:156007.666179.wzxl5em2LjckToAqLylJxzwXt3M; L=WnRGfUNYc2xfWkAFakRaBg5bYgN0cAVxGAcNIUANFBMvBwIAJw==.1587567800.14211.321094.42a247e10a7e1c492bb9a0954f71a90a; yandex_login=nick.kurkurin; skid=7141896761587577073; _ym_d=1587577075; visits=1586198458-1587549787-1587643024; lr=0; dcm=1; _ym_wasSynced=%7B%22time%22%3A1587643027213%2C%22params%22%3A%7B%22eu%22%3A0%7D%2C%22bkParams%22%3A%7B%7D%7D; _ym_isad=2; ugcp=1; yp=1581234933.com.3#1581437288.dq.1#1896019512.multib.1#1584131668.oyu.4775578911579527928#1596427321.szm.1_25%3A1536x864%3A1536x760#1902927800.udn.cDpuaWNrLmt1cmt1cmlu#1612368485.ygu.1#1894898590.yrtsi.1579538590#1587729427.yu.4775578911579527928; ymex=1590235027.oyu.4775578911579527928#1898004678.yrts.1582644678#1894898590.yrtsi.1579538590; oMaSofD=1; oMaFifD=1; spravka=dD0xNTg3NjQzNDY5O2k9MjE3LjE5OS4yMzYuMTM1O3U9MTU4NzY0MzQ2OTI1OTAwNzM2MztoPTNkZTAxZGNhNGU2NGU2MzFjNDM5MDRkYWEwODZlNWJl; uid=AABcdl6h0xFT/QBoBj78Ag==; js=1; first_visit_time={datetime.datetime.now().strftime("%Y-%m-%d")}T{datetime.datetime.now().strftime("%H")}%3A{datetime.datetime.now().strftime("%M")}%3A{datetime.datetime.now().strftime("%S")}%2B03%3A00; fonts-loaded=1; _ym_visorc_160656=b; _ym_visorc_45411513=b; metrika_enabled=1; _ym_metrika_enabled=1; HISTORY_AUTH_SESSION=edf01ebb; parent_reqid_seq=32a2e5f8f7b07ef01b1b51f5713261f1%2Cf56f71597835f3c7d2890719dde73a35%2C5929f9d6bc20b3dc7e7110c643dcfa53%2C4ccf49d0f29fea9142486001ae159458; _ym_metrika_enabled_160656=1; _ym_metrika_enabled_44910898=1; _ym_metrika_enabled_45411513=1; oMaPrfD=1'
     }
     st = datetime.datetime.now()
     i = 0
@@ -52,10 +64,9 @@ def parse(dict_of_product):
             all_proxies = parser_proxy.parse_proxy()
         else:
             i += 1
+        headers['Referer'] = product[1][:product[1].find('?')]
         html, title, url = '', product[0].strip(), product[1][:product[1].find('?')] + '/spec'
         print(f"Процесс - {index + 1}/{len(dict_of_product.keys())}", title, url)
-        # with open('process.txt', 'w') as f:
-        #     f.write(f"{index + 1}/{len(dict_of_product.keys())}")
         start = datetime.datetime.now()
         while html.find(title) == -1:
             proxies = {'https': all_proxies[ri(0, len(all_proxies) - 1)]}
@@ -64,6 +75,7 @@ def parse(dict_of_product):
                 html = requests.get(url, headers=headers, proxies=proxies).text
             except requests.exceptions.ProxyError:
                 print('Ошибка из-за неверного прокси, продолжаю работу;')
+                time.sleep(3)
                 continue
             else:
                 print('Времени прошло - ', datetime.datetime.now() - start)
--- main_pack/parser_proxy.py
+++ main_pack/parser_proxy.py
@@ -4,18 +4,10 @@ import requests
 
 def parse_proxy():
     html = requests.get('https://www.ip-adress.com/proxy-list').text
-    html = html[html.find('<table class="htable proxylist">'):]
     urls = list()
-    html = html[html.find('<a href="'):]
-    while re.search(r'title="More information about.*>.*</a>.*</td>', html) is not None:
-        new_url = re.search(r'">.*</a>.*</td>', html).group()[2:-5]
-        html = html[5:]
-        new_url = new_url[:new_url.find('</a')] + new_url[new_url.rfind(':'):]
-        urls.append(new_url)
-        html = html[html.find('<a href'):]
-    all_urls = re.findall(r'<a href="https:\/\/www\.ip-adress\.com\/ip-address\/.*<\/td>', html)
-    for proxy in all_urls:
-        urls.append(re.sub(r'</a>', '', re.search(r'">.*</td', proxy).group()[2:-4]))
+    for i in re.finditer(r'(\d{1,3}.){4}</a>:\d{1,4}</td>\n<td>transparent</td>', html):
+        withouttrans = i.group()[:-26]
+        urls.append(withouttrans[:withouttrans.find('<')] + withouttrans[withouttrans.rfind('>') + 1:])
     return tuple(urls)
 
 
