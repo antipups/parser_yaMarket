ec7353ad4ab9530b07bd511aaf082c957b8b1396
antipups <nick.kurkurin@protonmail.com>
2020-04-26 17:43:30 +0300

    Вставленны и старые прокси.

--- main_pack/parser_proxy.py
+++ main_pack/parser_proxy.py
@@ -3,14 +3,13 @@ import requests
 
 
 def parse_proxy():
-    # html = requests.get('https://www.ip-adress.com/proxy-list').text
-    # urls = list()
-    # for i in re.finditer(r'(\d{1,3}.){4}</a>:\d{1,4}</td>\n<td>transparent</td>', html):
-    #     withouttrans = i.group()[:-26]
-    #     urls.append(withouttrans[:withouttrans.find('<')] + withouttrans[withouttrans.rfind('>') + 1:])
+    html = requests.get('https://www.ip-adress.com/proxy-list').text
+    urls = list()
+    for i in re.finditer(r'(\d{1,3}.){4}</a>:\d{1,4}</td>\n<td>transparent</td>', html):
+        withouttrans = i.group()[:-26]
+        urls.append(withouttrans[:withouttrans.find('<')] + withouttrans[withouttrans.rfind('>') + 1:])
 
     html = requests.get('https://free-proxy-list.net/').text
-    urls = list()
     for proxy in re.finditer(r'\d{1,3}\.((?!ago).)*', re.search(r'<tbody>.*</tbody>', html).group()):
         proxy = proxy.group()
         if re.search(r'hx.{4}', proxy).group().endswith('ye'):
