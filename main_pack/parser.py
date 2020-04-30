846d274a0e364c6ff21a44203173084b166fc378
antipups <nick.kurkurin@protonmail.com>
2020-04-30 17:33:26 +0300

    Добавление: 1) Сохранение картинки; 2) Вывод пути к картинке в excel;

--- main_pack/parser.py
+++ main_pack/parser.py
@@ -46,7 +46,7 @@ def parse(dict_of_product):
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'Cache-Control': 'max-age=0',
         'Connection': 'keep-alive',
-        'Cookie': '_ym_uid=1579538585539137365; mda=0; yandexuid=4775578911579527928; yuidss=4775578911579527928; i=7fpXxLZtVlknYALqfuHdIqESJ7QKPyrT/p1Ib1NKFr2+GlI6yQ4Nq8EuI7FxxNSTvC5JPW7DUgpjBnX2NCjgeBJhLQE=; my=YwA=; yandex_gid=142; gdpr=0; yandexmarket=48%2CUAH%2C1%2C%2C%2C%2C2%2C0%2C0%2C0%2C0%2C0%2C12%2C0; mOC=1; currentRegionId=142; currentRegionName=%D0%94%D0%BE%D0%BD%D0%B5%D1%86%D0%BA; cycada=vBIVPiL1OivB3zUy7Bun86lEsDT+JL2RpLjQnNHDsg4=; yabs-frequency=/4/0000000000000000/AKgmSB0wGG00/; oMaSefD=1; oMaSpfD=1; oMaRefD=1; Session_id=3:1587567800.5.0.1587567800047:uOvH2Q:4c.1|230517873.0.2|4:156007.208530.Ct9WborQwK5ZqYGCgMhyMgHKkSI; sessionid2=3:1587567800.5.0.1587567800047:uOvH2Q:4c.1|230517873.0.2|4:156007.666179.wzxl5em2LjckToAqLylJxzwXt3M; L=WnRGfUNYc2xfWkAFakRaBg5bYgN0cAVxGAcNIUANFBMvBwIAJw==.1587567800.14211.321094.42a247e10a7e1c492bb9a0954f71a90a; yandex_login=nick.kurkurin; skid=7141896761587577073; _ym_d=1587577075; oMaSofD=1; oMaFifD=1; spravka=dD0xNTg3NjQzNDY5O2k9MjE3LjE5OS4yMzYuMTM1O3U9MTU4NzY0MzQ2OTI1OTAwNzM2MztoPTNkZTAxZGNhNGU2NGU2MzFjNDM5MDRkYWEwODZlNWJl; oMaPrfD=1; _ym_isad=2; uid=AABcfl6l4rNcxwB8FlvSAg==; js=1; _ym_wasSynced=%7B%22time%22%3A1587929780582%2C%22params%22%3A%7B%22eu%22%3A0%7D%2C%22bkParams%22%3A%7B%7D%7D; yp=1581234933.com.3#1581437288.dq.1#1896019512.multib.1#1584131668.oyu.4775578911579527928#1596427321.szm.1_25%3A1536x864%3A1536x760#1902927800.udn.cDpuaWNrLmt1cmt1cmlu#1612368485.ygu.1#1894898590.yrtsi.1579538590#1588016182.yu.4775578911579527928; ymex=1590521782.oyu.4775578911579527928#1898004678.yrts.1582644678#1894898590.yrtsi.1579538590; first_visit_time=2020-04-26T22%3A36%3A25%2B03%3A00; fonts-loaded=1; visits=1586198458-1587888784-1587974766; lr=0; dcm=1; HISTORY_AUTH_SESSION=bb8d1fce; metrika_enabled=1; _ym_metrika_enabled=1; _ym_visorc_160656=b; ugcp=1; _ym_visorc_45411513=b; _ym_metrika_enabled_160656=1; _ym_metrika_enabled_44910898=1; _ym_metrika_enabled_45411513=1; parent_reqid_seq=1ab7b7d5dc7b3f90d261fa9c4ea890f5%2C64b51b361cb8f2d81d2f8a19ce18d007',
+        'Cookie': '_ym_uid=1579538585539137365; mda=0; yandexuid=4775578911579527928; yuidss=4775578911579527928; i=7fpXxLZtVlknYALqfuHdIqESJ7QKPyrT/p1Ib1NKFr2+GlI6yQ4Nq8EuI7FxxNSTvC5JPW7DUgpjBnX2NCjgeBJhLQE=; my=YwA=; yandex_gid=142; gdpr=0; yandexmarket=48%2CUAH%2C1%2C%2C%2C%2C2%2C0%2C0%2C0%2C0%2C0%2C12%2C0; mOC=1; currentRegionId=142; currentRegionName=%D0%94%D0%BE%D0%BD%D0%B5%D1%86%D0%BA; yabs-frequency=/4/0000000000000000/AKgmSB0wGG00/; oMaSefD=1; oMaSpfD=1; oMaRefD=1; Session_id=3:1587567800.5.0.1587567800047:uOvH2Q:4c.1|230517873.0.2|4:156007.208530.Ct9WborQwK5ZqYGCgMhyMgHKkSI; sessionid2=3:1587567800.5.0.1587567800047:uOvH2Q:4c.1|230517873.0.2|4:156007.666179.wzxl5em2LjckToAqLylJxzwXt3M; L=WnRGfUNYc2xfWkAFakRaBg5bYgN0cAVxGAcNIUANFBMvBwIAJw==.1587567800.14211.321094.42a247e10a7e1c492bb9a0954f71a90a; yandex_login=nick.kurkurin; skid=7141896761587577073; _ym_d=1587577075; oMaSofD=1; oMaFifD=1; oMaPrfD=1; _ym_isad=2; _ym_wasSynced=%7B%22time%22%3A1588195331685%2C%22params%22%3A%7B%22eu%22%3A0%7D%2C%22bkParams%22%3A%7B%7D%7D; yp=1581234933.com.3#1581437288.dq.1#1896019512.multib.1#1584131668.oyu.4775578911579527928#1596427321.szm.1_25%3A1536x864%3A1536x760#1902927800.udn.cDpuaWNrLmt1cmt1cmlu#1612368485.ygu.1#1894898590.yrtsi.1579538590#1588281734.yu.4775578911579527928; ymex=1590787334.oyu.4775578911579527928#1898004678.yrts.1582644678#1894898590.yrtsi.1579538590; _ym_visorc_10630330=b; _ym_metrika_enabled_10630330=1; spravka=dD0xNTg4MTk1MzQ1O2k9MjE3LjE5OS4yMzYuMTUxO3U9MTU4ODE5NTM0NTUyNjA3Mzk0MTtoPTlmOTRiOTA5ZGQ5ZjUxY2U5YWY2NzAxMmJmZWE5YTRh; visits=1586198458-1587974766-1588195345; lr=0; parent_reqid_seq=7634b215cd9e2d690e76b247a5daee8a; uid=AABcfl6p8BGq2wBoBp5QAg==; js=1; dcm=1; metrika_enabled=1; _ym_metrika_enabled=1; _ym_visorc_160656=b; first_visit_time=2020-04-30T00%3A22%3A26%2B03%3A00; HISTORY_AUTH_SESSION=880ba9a1; fonts-loaded=1; _ym_visorc_45411513=b; ugcp=1; _ym_metrika_enabled_160656=1; _ym_metrika_enabled_44910898=1; _ym_metrika_enabled_45411513=1',
         'Host': 'market.yandex.ua',
         'Sec-Fetch-Dest': 'document',
         'Sec-Fetch-Mode': 'navigate',
@@ -93,7 +93,6 @@ def parse_all_pic(html):
     pic_code = re.search(r'{\"property\":\"og:image\",\"content\":\".*twitter:image', html)
     if not pic_code:
         return False
-
     if not os.path.exists('image'):
         os.mkdir('image')
 
@@ -104,7 +103,7 @@ def parse_all_pic(html):
         pic = pic.group()
         if pic not in all_pic:
             all_pic.append(pic)
-            filename = 'image\\' + str(index) + '.png'
+            filename = 'image\\' + datetime.datetime.now().strftime('%Y%m%d%H%M%S%f') + '.png'
             all_name_of_pic.append(filename)
             with open(filename, 'wb') as f:
                 f.write(requests.get(pic).content)
@@ -145,6 +144,7 @@ def write_xlsx(dict_of_info, tuple_of_pic):
         if 'Лист1' not in wb.sheetnames:
             wb.create_sheet('Лист1')
         ws = wb['Лист1']
+        ws.cell(row=2, column=2).value = 'Путь к картинке'
         ws.column_dimensions['a'].width = 30
         i = 2
         dict_of_col = {}
@@ -167,6 +167,7 @@ def write_xlsx(dict_of_info, tuple_of_pic):
             img.save(pic)
         img = openpyxl.drawing.image.Image(pic)
         ws.add_image(img, 'A' + str(row + 11 * index))
+        ws.cell(column=2, row=row + 1 + 11 * index).value = pic
     else:
         ws.cell(row=row + 1 + 11 * index, column=1).value = 'Максимальная строка'
 
@@ -183,8 +184,6 @@ def write_xlsx(dict_of_info, tuple_of_pic):
         else:
             wb.close()
             print('Закрыл 2.xlsx.')
-            for pic in tuple_of_pic:
-                os.remove(pic)
             return
 
 
